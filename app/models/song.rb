fb379de96aae8374b6bcf86eeff25c34e0ce7f4c
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-27 23:36:48 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,11 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
   def artist_name
-    if self.artist 
-      self.artist.name
-    end
+     self.artist ? self.artist.name : nil
   end
 end
